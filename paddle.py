3b4923463cb064901aa209bfefef9f6188149e9f
alvar <alvarnivert@gmail.com>
2025-03-31 18:20:26 +0200

    made paddle figures

--- paddle.py
+++ paddle.py
@@ -12,9 +12,8 @@ class Paddle():
     def figure(self):
         figure = [] # corners
         side = self.side_on_rectangle(self.x,self.y,self.bound)
-        n_side = (side + 1) % 4
+        n_side = (side + 1) % 4 # next side/ point,
         a = 1
-        print(side)
         if side >1:
             a = -1
         if side == 0 or side == 2: # upp and down
@@ -30,7 +29,7 @@ class Paddle():
                 figure.append((self.bound[n_side][0] - a*self.height/2 , self.bound[n_side][1] + a*abs(self.bound[n_side][0] - self.x) ))
                 figure.append((self.bound[n_side][0] - a*self.height/2 , self.bound[n_side][1] + a*self.height/2)) # inner corner
 
-            figure.append((self.x, self.y + a*self.height / 2))
+            figure.append((self.x, self.y + a*self.height / 2)) #last corner
 
         elif side == 1 or side == 3: # left and right
             figure.append((self.x + a*self.height/2 ,self.y)) # 1 corner
@@ -45,7 +44,7 @@ class Paddle():
                 figure.append((self.bound[n_side][0] - a*abs(self.bound[n_side][0] - self.y) , self.bound[n_side][1] - a*self.height/2))
                 figure.append((self.bound[n_side][0] - a*self.height/2 , self.bound[n_side][1] - a*self.height/2)) # inner corner
 
-            figure.append((self.x - a*self.height / 2, self.y))
+            figure.append((self.x - a*self.height / 2, self.y)) # last corner
         return figure
 
     # find witch side the point is on
@@ -57,7 +56,7 @@ class Paddle():
                 return i
         return False
 
-    # Function to check if a point is on a line segment
+    # check if a point is on a line segment
     def is_point_on_line(self,px, py, x1, y1, x2, y2):
         # Check collinearity
         if (py - y1) * (x2 - x1) != (px - x1) * (y2 - y1):
