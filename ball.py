9f78275b125f5d881d53c2e5f4ff8f77041baa9e
set2ok <07alvniv@gafe.molndal.se>
2025-03-31 14:49:16 +0200

    create

--- ball.py
+++ ball.py
@@ -1,6 +1,17 @@
+import math
 class Ball():
     def __init__(self,pos,radius, speed = 0, direction = 0):
         self.x, self.y = pos
         self.radius = radius
         self.speed = speed
-        direction = direction
+        self.direction = direction
+
+    def colision(self, objects):
+        for obj in objects:
+            if (self.x + self.radius) >= obj.x + obj.lenght/2 and obj.x - obj.lenght/2 >= (self.x - self.radius) and (
+                    self.y + self.radius) >= obj.y >= (self.x - self.radius)
+
+
+    def update(self, dt):
+        self.x += math.cos(self.direction)* self.speed * dt
+        self.x += math.sin(self.direction) * self.speed * dt
--- paddle.py
+++ paddle.py
@@ -1,7 +1,27 @@
 class Paddle():
 
-    def __init__(self,pos, width, height, state = "hor"):
+    def __init__(self,pos, width, height, bound):
         self.x,self.y = pos
         self.width = width
         self.height = height
-        self.state = state # hor and ver, horizontal, vertical
+        self.bound = bound # [(x,y) * 4]
+
+    def bounds(self):
+        bunds = []
+
+    def point_on_rectangle(self,px, py, rect_points):
+        for i in range(4):
+            x1, y1 = rect_points[i]
+            x2, y2 = rect_points[(i + 1) % 4]  # Wrap around to form edges
+            if self.is_point_on_line(px, py, x1, y1, x2, y2):
+                return True
+        return False
+
+    # Function to check if a point is on a line segment
+    def point_on_line(self,px, py, x1, y1, x2, y2):
+        # Check collinearity
+        if (py - y1) * (x2 - x1) != (px - x1) * (y2 - y1):
+            return False
+        # Check if point is within segment bounds
+        if x1 ==x2:
+            return min(x1, x2) <= px <= max(x1, x2) and min(y1, y2) <= py <= max(y1, y2)
